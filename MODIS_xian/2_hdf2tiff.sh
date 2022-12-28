cbba105a0999696a3a4149aa3cb441f4e5a84e2b
SauryCHEN <61134581+SauryChen@users.noreply.github.com>
2022-12-28 10:54:20 +0800

    Update 2_hdf2tiff.sh

--- MODIS_xian/2_hdf2tiff.sh
+++ MODIS_xian/2_hdf2tiff.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-source_dir="/data1/zjp/MODIS/xian_data_cmx/"
-save_dir="/data1/zjp/MODIS/xian_data_cmx/data_process/"
+
+# to run the program: bash modis_process.sh
+source_dir="/ceph-data/cmx/xian/data/xian_data_cmx/"
+save_dir="/ceph-data/cmx/xian/data/xian_data_cmx/data_process/"
 
 for file in `ls $source_dir`
 do
     if [[ "${file##*.}" == "hdf" ]];then
         echo $file
-        gdal_translate $file -sds ${save_dir}${file: 0: 23}.tif
-        gdal_merge.py -separate -o ${save_dir}${file: 0: 23}.tif ${save_dir}${file: 0: 23}_*.tif
-        rm -rf ${save_dir}*h26v05_*.tif ${save_dir}*h27v05_*.tif
+	gdal_translate $file -sds ${save_dir}${file: 0: 23}.tif
+	gdal_merge.py -separate -o ${save_dir}${file: 0: 23}.tif ${save_dir}${file: 0: 23}_01.tif ${save_dir}${file: 0: 23}_02.tif ${save_dir}${file: 0: 23}_03.tif ${save_dir}${file: 0: 23}_04.tif ${save_dir}${file: 0: 23}_05.tif ${save_dir}${file: 0: 23}_06.tif ${save_dir}${file: 0: 23}_07.tif ${save_dir}${file: 0: 23}_08.tif ${save_dir}${file: 0: 23}_09.tif ${save_dir}${file: 0: 23}_10.tif ${save_dir}${file: 0: 23}_11.tif ${save_dir}${file: 0: 23}_12.tif
+	rm -rf ${save_dir}*h26v05_*.tif ${save_dir}*h27v05_*.tif
     fi
 done
